a9ab957528af9b9184e00e5d585bbbdbfd0cda4f
Bernat Borrás Paronella <bernatbor15@gmail.com>
2015-01-31 04:05:04 +0100

    Merge

--- app/src/main/java/com/alorma/github/ui/fragment/base/BaseListFragment.java
+++ app/src/main/java/com/alorma/github/ui/fragment/base/BaseListFragment.java
@@ -81,7 +81,7 @@ public abstract class BaseListFragment extends Fragment implements AbsListView.O
 				fab.setSize(FloatingActionButton.SIZE_NORMAL);
 				GithubIconDrawable drawable = new GithubIconDrawable(getActivity(), getFABGithubIcon()).color(Color.WHITE).fabSize();
 
-				fab.setIconDrawable(drawable);
+				fab.setDrawable(drawable);
 			} else {
 				fab.setVisibility(View.GONE);
 			}
